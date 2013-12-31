0254f30d4577fa5d048d9f60bb5f995c19c7fefd
Stefan Karlsson <stefan.karlsson@gmail.com>
2013-12-31 14:08:50 +0100

    Add abstraction to i2c_helper.h to support both dsPIC33 (I2C1) and dsPIC30 (I2C)

--- src/communication/i2c_helper.h
+++ src/communication/i2c_helper.h
@@ -7,51 +7,103 @@
 
 #include <i2c.h>
 
+#ifdef _MI2CIF
+#define StartI2C_       StartI2C
+#define RestartI2C_     RestartI2C
+#define StopI2C_        StopI2C
+#define MasterWriteI2C_ MasterWriteI2C
+#define MasterReadI2C_  MasterReadI2C
+#define AckI2C_         AckI2C
+#define MI2CIF_         _MI2CIF
+#define I2CCONbits_     I2CCONbits
+#define I2C1STATbits_   I2CSTATbits
+#elif defined(_MI2C1IF)
+#define StartI2C_       StartI2C1
+#define RestartI2C_     RestartI2C1
+#define StopI2C_        StopI2C1
+#define MasterWriteI2C_ MasterWriteI2C1
+#define MasterReadI2C_  MasterReadI2C1
+#define AckI2C_         AckI2C1
+#define MI2CIF_         _MI2C1IF
+#define I2CCONbits_    I2C1CONbits
+#define I2CSTATbits_   I2C1STATbits
+#endif
+
 // Current implementation waits in spin loops.
 // TODO: Add support for interrupt based waits.
 // TODO: Add some error handling. The current implementation
 //       hangs if the slaves don't answer as expected.
 
+#ifndef I2C_ERROR
+#define I2C_ERROR(message)
+#endif
+
 void i2c_start_and_wait() {
-    StartI2C();
+    StartI2C_();
     // Wait till Start sequence is completed
-    while (I2CCONbits.SEN);
+    while (I2CCONbits_.SEN);
+
+    if (I2CSTATbits_.BCL) {
+        I2C_ERROR("Bus collision in i2c_start_and_wait\n");
+    }
     // Clear interrput flag
-    IFS0bits.MI2CIF = 0;
+    MI2CIF_ = 0;
 }
 
 void i2c_restart_and_wait(void) {
-    RestartI2C();
-    while (I2CCONbits.RSEN);
-    IFS0bits.MI2CIF = 0;
+    RestartI2C_();
+    while (I2CCONbits_.RSEN);
+    MI2CIF_ = 0;
 }
 
 void i2c_stop_and_wait() {
     // Stop the data write
-    StopI2C();
+    StopI2C_();
     // Wait till stop sequence is completd
-    while (I2CCONbits.PEN);
+    while (I2CCONbits_.PEN);
+
+
+    while (!MI2CIF_);
+    MI2CIF_ = 0;
+
+    if (I2CSTATbits_.BCL) {
+        I2C_ERROR("Bus collision in i2c_stop_and_wait\n");
+    }
 }
 
 void i2c_write_and_wait_no_ack(unsigned char value) {
-    MasterWriteI2C(value);
-    while (I2CSTATbits.TBF);
-    while (!IFS0bits.MI2CIF);
-    IFS0bits.MI2CIF = 0;
+    char ret = MasterWriteI2C_(value);
+    if (ret == -1) {
+        debug_blink(0, 2000, 500);
+    }
+    while (I2CSTATbits_.TBF);
+    while (!MI2CIF_);
+    MI2CIF_ = 0;
 }
 
 void i2c_write_and_wait(unsigned char value) {
-    MasterWriteI2C(value);
-    while (I2CSTATbits.TBF);
-    while (!IFS0bits.MI2CIF);
-    IFS0bits.MI2CIF = 0;
-    while (I2CSTATbits.ACKSTAT);
+    char ret = MasterWriteI2C_(value);
+    if (ret != 0) {
+        I2C_ERROR("MasterWriteI2C failed\n");
+    }
+    while (I2CSTATbits_.TBF);
+    while (!MI2CIF_);
+    MI2CIF_ = 0;
+    while (I2CSTATbits_.ACKSTAT);
 }
 
 unsigned char i2c_read_and_wait(void) {
-    unsigned char c = MasterReadI2C();
-    AckI2C();
-    while (I2CCONbits.ACKEN);
+    unsigned char c = MasterReadI2C_();
+    AckI2C_();
+    while (I2CCONbits_.ACKEN);
+
+    while (!MI2CIF_);
+    MI2CIF_ = 0;
+
+    if (I2CSTATbits_.BCL) {
+        I2C_ERROR("Bus collision in i2c_read_and_wait\n");
+    }
+
     return c;
 }
 
@@ -60,12 +112,12 @@ void i2c_ack_poll(unsigned char value) {
     i2c_start_and_wait();
     i2c_write_and_wait_no_ack(value);
 
-    while (I2CSTATbits.ACKSTAT) {
+    while (I2CSTATbits_.ACKSTAT) {
         i2c_restart_and_wait();
         i2c_write_and_wait_no_ack(value);
     }
 
-    i2c_stop_and_wait();
+    i2c_stop_and_wait(99);
 }
 
 #endif	/* I2C_HELPER_H */

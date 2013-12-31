262ba9d4c26feb4c4086af34df2b217608d08776
Stefan Karlsson <stefan.karlsson@gmail.com>
2013-12-31 14:17:06 +0100

    Use Microstick II LED when debug blinking

--- src/debug/led_debug.h
+++ src/debug/led_debug.h
@@ -4,13 +4,24 @@
 #include "xc.h"
 
 // FIXME: Hard coded for now.
+#define MICROSTICK2
+
+#ifdef MICROSTICK2
+#define DEBUG_BIT_0 LATAbits.LATA0
+#define DEBUG_BIT_1 LATAbits.LATA0
+#else
 #define DEBUG_BIT_0 LATCbits.LATC13
 #define DEBUG_BIT_1 LATCbits.LATC14
+#endif
 
 inline static void debug_init(void) {
     // Set to output pins.
+#ifdef MICROSTICK2
+    TRISAbits.TRISA0 = 0;
+#else
     TRISCbits.TRISC13 = 0;
     TRISCbits.TRISC14 = 0;
+#endif
 }
 
 // Hang and blink a led.
